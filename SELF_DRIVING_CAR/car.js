c47ee8ddfa11c1f64d9eddfd43bb06ed2563212f
Leonaitasi Mafileo <leonaitasim@gmail.com>
2025-07-05 00:07:04 +1200

    ERROR: 500 AI car sensors not all detecting -Some of the 500 AI cars sensors are not working to see a border or dummy car when hitting straightforward and crash

--- SELF_DRIVING_CAR/car.js
+++ SELF_DRIVING_CAR/car.js
@@ -116,10 +116,10 @@ class Car{
         if(this.speed!=0){
             const flip=this.speed>0?1:-1;
             if(this.controls.left){
-                this.angle+=0.03*flip;
+                this.angle += 0.015 * flip; // Smaller angle = smoother turn
             }
             if(this.controls.right){
-                this.angle-=0.03*flip;
+                this.angle-=0.015*flip;
             }
         }
         
