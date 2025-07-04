d66415797f2296555f404000eac2913ba42a8e65
Leonaitasi Mafileo <leonaitasim@gmail.com>
2025-07-04 23:29:53 +1200

    Genetic algorithm

--- SELF_DRIVING_CAR/main.js
+++ SELF_DRIVING_CAR/main.js
@@ -8,12 +8,18 @@ const networkCtx = networkCanvas.getContext("2d");
 
 const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
 
-const N=100;
+const N=500;
 const cars=generateCars(N);
 let bestCar=cars[0];
 if(localStorage.getItem("bestBrain")){
-    bestCar.brain=JSON.parse(
+    for(let i=0;i<cars.length;i++){
+        cars[i].brain=JSON.parse(
         localStorage.getItem("bestBrain"));
+        if(i!=0){
+            NeuralNetwork.mutate(cars[i].brain,0.2);
+        }
+    }
+    
 }
 
 const traffic=[
